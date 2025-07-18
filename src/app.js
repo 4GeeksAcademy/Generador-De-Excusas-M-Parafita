deeacba9bad1faccbc7d5f273c846f30bff3c85f
Martin <90578316+MartinParafita@users.noreply.github.com>
2025-07-18 18:56:54 +0000

    excusas terminadas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  function selector(who,action,what,when) {
+    let nuevaExcusa = "";
+    let random = Math.floor(Math.random() * 4);
+    nuevaExcusa += who[random]+" ";
+    random = Math.floor(Math.random() * 4);
+    nuevaExcusa += action[random]+" ";
+    random = Math.floor(Math.random() * 4);
+    nuevaExcusa += what[random]+" ";
+    random = Math.floor(Math.random() * 4);
+    nuevaExcusa += when[random]+" ";
+    return nuevaExcusa;
+  }
+  let excusa = selector(who,action,what,when);
+  let parrafoHTML = document.getElementById("excuse");
+  parrafoHTML.innerHTML = excusa
 };
